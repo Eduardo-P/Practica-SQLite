17c9a5903588ad342cca3fc61f085d6a00257cfd
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-25 18:33:00 -0500

    Archivo client.js con el evento que se activara al hacer click en el boton buscar, obteniendo los valores de los campos donde el usuario ingresara lo que va a buscar

---
+++ public/client.js
@@ -0,0 +1,11 @@
+document.getElementById("search").addEventListener("click", function() {
+    search();
+});
+
+function search() {
+    const name = document.getElementById('name').value;
+    const movie_title = document.getElementById('movie_title').value;
+    const year = document.getElementById('year').value;
+    const score = document.getElementById('score').value;
+    const votes = document.getElementById('votes').value;
+}
