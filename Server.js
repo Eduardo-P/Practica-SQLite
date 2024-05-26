ebaaf53c85586a8d14b574e573543fcee849d3bf
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-25 19:02:00 -0500

    Agregando modulos al script del servidor, abriendo en el puerto 3000, ademas de redirecionamiento a index.html

---
+++ Server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bp = require('body-parser')
+const path = require('path')
+const app = express()
+
+app.use(express.static('public'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+	extended: true
+}))
+
+app.listen(3000, () => {
+	console.log("Escuchando en: http://localhost:3000")
+})
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'))
+})
