4131f1a295c807f09f2956a9433e63c0d6279e3c
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-25 19:26:00 -0500

    ERROR: Intento de conexion a la base de datos - al parecer el modulo sqlite3 no ha sido importado

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-const express = require('express') // ERROR - modulo express no se puede encontrar
+const express = require('express')
 const bp = require('body-parser')
 const path = require('path')
 const app = express()
@@ -16,3 +16,12 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'))
 })
+
+// Conectar a la base de datos SQLite
+const db = new sqlite3.Database('imdb.db', (err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err.message);
+        return;
+    }
+    console.log('Conectado a la base de datos SQLite');
+});
