7054eea05ebbdf40619b8b80c359363c11f15599
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-25 19:37:00 -0500

    CORRECCION: Se instalo sqlite3 en el proyecto

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const sqlite3 = require('sqlite3').verbose(); // ERROR - modulo sqlite3 no esta instalado
+const sqlite3 = require('sqlite3').verbose();
 const bp = require('body-parser')
 const path = require('path')
 const app = express()
